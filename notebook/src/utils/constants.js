1f58a7f040c4c4934ed53596ae67680ba91b38b0
sambantham <76392855+sambantham-p@users.noreply.github.com>
2024-12-29 23:09:52 +0530

    Update constants.js

--- notebook/src/utils/constants.js
+++ notebook/src/utils/constants.js
@@ -1,2 +1,2 @@
 //export const BASE_URL = 'http://localhost:8000';
-export const BASE_URL = 'https://notebook-project-backend.onrender.com'
+export const BASE_URL = 'https://notebook-project-7h2l.onrender.com/'
